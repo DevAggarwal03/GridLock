22920406fa6380a9b3ebbabf04a90d088aff15b0
Dev Aggarwal <146590584+DevAggarwal03@users.noreply.github.com>
2025-09-28 09:03:20 +0530

    Update index.ts

--- apps/http-server/src/controllers/resultAnnounce/index.ts
+++ apps/http-server/src/controllers/resultAnnounce/index.ts
@@ -4,7 +4,7 @@ import { abi, contractAddress } from "./info";
 import { MatchSchema } from "@repo/types";
 
 const privateKey = process.env.PRIVATE_KEY!;
-const provider = new ethers.JsonRpcProvider("https://sepolia-rollup.arbitrum.io/rpc")
+const provider = new ethers.JsonRpcProvider("https://testnet.evm.nodes.onflow.org")
 const wallet = new ethers.Wallet(privateKey,provider);
 const contract = new ethers.Contract(contractAddress,abi,wallet);
 
